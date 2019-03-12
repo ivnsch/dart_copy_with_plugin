43e9b1d54c7373b72884a9458a8073639c5c12a4
Ivan Schuetz <ivanschuetz@gmail.com>
2019-03-12 11:58:31 +0100

    Show action only in Dart files

--- src/main/kotlin/action/GenerateCopyWithMethodAction.kt
+++ src/main/kotlin/action/GenerateCopyWithMethodAction.kt
@@ -4,13 +4,15 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
 
 class GenerateCopyWithMethodAction : AnAction("Generate copyWith") {
 
     override fun update(event: AnActionEvent) {
-        val project = event.project
         val editor = event.getData(CommonDataKeys.EDITOR)
-        event.presentation.isVisible = project != null && editor != null
+        event.presentation.isVisible =
+            event.project != null // should not be null if there's a file extension, but let's be sure
+            && editor?.let { FileDocumentManager.getInstance().getFile(it.document)?.extension } == "dart"
     }
 
     override fun actionPerformed(event: AnActionEvent) {
